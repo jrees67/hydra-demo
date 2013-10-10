546ad402c2666dc1026f64f4b76ac12dc81d483f
John Rees <jres67@yahoo.com>
2013-10-10 11:28:50 -0400

    Added rspec

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,7 +2,7 @@ class Book < ActiveFedora::Base
   has_metadata 'descMetadata', type: BookMetadata
   has_many :pages, :property=> :is_part_of
 
-  delegate :title, to: 'descMetadata', unique: true
+  delegate :title, to: 'descMetadata', unique: false
   delegate :author, to: 'descMetadata', unique: false
 
 
